a6b54a9fef26f4c83d8899badd3abbc28484502c
Napa <napa_tom@hotmail.com>
2017-06-12 01:29:05 +0700

    load env

--- public/index.php
+++ public/index.php
@@ -6,16 +6,10 @@ use LINE\Core\Setting;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-var_dump(__DIR__ . '/../');
-
-$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
 $dotenv->load();
 
 $setting = Setting::getSetting();
-
-var_dump($setting);
-
-die(1);
 
 $app = new \Slim\App($setting);
 (new Route())->register($app);
